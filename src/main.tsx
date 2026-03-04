d4ee55f49fa0367ce9e3c5129235e7c400dda9be
Jovan1-maker <negneg1231231232gmail.com>
2026-03-04 21:17:44 +0800

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-﻿import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'  // your Tailwind CSS file
+import './index.css' // Tailwind CSS entry point
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -12,10 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-=======
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
->>>>>>> 9afc44d0c8ffebbb5b60b3b379e54663283bde89
